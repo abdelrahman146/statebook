1e77419464841aeb7b9a57c3507c3dde942a1b38
Abdel Rahman Hussein <abdelrahman146@outlook.com>
2022-07-03 19:49:33 +0400

    removed ref

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {statebook, useGlobalStatebook} from 'statebook';
